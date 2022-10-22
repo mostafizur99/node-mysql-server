ba92db2cf49ee4b96be064fe0a1d520e5315f3ba
mostafizur99 <coder.mr11@gmail.com>
2022-10-22 18:53:28 +0600

    feat: mysql connection added

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-console.log('Hello MySQL')
+var mysql = require('mysql');
+require('dotenv').config()
+
+const dbConnectionConfig = {
+    host: 'localhost',
+    user: process.env.MYSQL_DB_USER,
+    password: process.env.MYSQL_DB_PASSWORD,
+    database: process.env.MYSQL_DB_DATABASE,
+}
+
+const connection = mysql.createConnection(dbConnectionConfig);
+
+connection.connect((err) => {
+    if (err) {
+        console.error('DB Connection Error: ' + err.stack);
+    } else {
+        console.log('DB Connected');
+    }
+});
